2efa679b9f676a900bab5d42edd575124ee8e525
Volodymyr <vladimir.savchenko@gmail.com>
2019-08-22 11:56:19 +0200

    renamed

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 
 setup(
         name='oda-workflows',
-        version='1.0.1',
-        py_modules= ['odaw','service_exception'],
+        version='1.0.2',
+        py_modules= ['workflows','service_exception'],
         url="http://odahub.io",
         package_data     = {
             "": [
@@ -15,7 +15,7 @@ setup(
                 ]
             },
         entry_points = {
-            'console_scripts': ['ew=odaw:evaluate_console'],
+            'console_scripts': ['ew=workflows:evaluate_console'],
         },
         license='Creative Commons Attribution-Noncommercial-Share Alike license',
         long_description=open('README.md').read(),
