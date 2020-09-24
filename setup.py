8c0bfc96501d7eb2d5dd836f02e5025f027da091
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-09-24 15:18:44 +0200

    Bump version: 1.3.25 → 1.3.26

--- setup.py
+++ setup.py
@@ -15,5 +15,5 @@ __doc__ = ast.get_docstring(mod)
 
 setup(description=__doc__.splitlines()[1],
       long_description=__doc__,
-      version = "1.3.25", 
+      version = "1.3.26", 
       setup_requires=setup_requires)
