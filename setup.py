c32e258e73909c32beb617b7c1c419113530ef88
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2020-11-11 15:49:40 +0100

    Bump version: 1.3.28 → 1.3.29

--- setup.py
+++ setup.py
@@ -15,5 +15,5 @@ __doc__ = ast.get_docstring(mod)
 
 setup(description=__doc__.splitlines()[1],
       long_description=__doc__,
-      version = "1.3.28", 
+      version = "1.3.29", 
       setup_requires=setup_requires)
