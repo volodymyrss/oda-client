d7c5d366ce07ddcdb027faad65474e3392e53178
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2021-11-01 16:56:54 +0100

    Bump version: 1.3.37 → 1.3.38

--- setup.py
+++ setup.py
@@ -15,5 +15,5 @@ __doc__ = ast.get_docstring(mod)
 
 setup(description=__doc__.splitlines()[1],
       long_description=__doc__,
-      version = "1.3.37", 
+      version = "1.3.38", 
       setup_requires=setup_requires)
